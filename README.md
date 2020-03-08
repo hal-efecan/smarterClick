a12fc60abf0b0c30c06085eda713d89e9f6dd2f3
hal-efecan <hefecan@gmail.com>
2020-03-08 19:41:44 +0000

    additional refactoring & comment additions

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 To demonstrate DOM manipulation and traversal using **vanilla JavaScript**
 
+**Part 1** - Adding CSS and HTML to the page dynamically  
+**Part 2** - Element Hover  
+**Part 3** - Getting the elements path  
+**Part 4** - Putting it all together  
+
+Running your script will now mean any element that you hover over will have a red outline, and clicking on it will show it's selected path at the bottom of the page.
+
 ### **Resources used**:  
 
 [MDN Web docs](https://https://developer.mozilla.org/en-US/ "MDN Web docs")  
--- javascript/app.js
+++ javascript/app.js
@@ -1,103 +1,96 @@
+// Part 1 - Adding CSS and HTML to the page dynamically
 function addCssToStyleTag() {
     // Create style elementTag
-    const styleTag = document.createElement('style')
+    const style = document.createElement('style')
     // + set attr 
-    styleTag.setAttribute('id', 'smc-hover-css')
+    style.setAttribute('id', 'smc-hover-css')
     // + css Styling
-    styleTag.textContent = "smc-path-box {" +
-    "position: fixed;" +
-    "bottom: 20px;" +
-    "left: 10px;" +
-    "right: 10px;" +
-    "padding: 3px;" +
-    "font-size: 16px;" +
-    "background-color: rgba(87, 12, 12, 0.8);" +
-    "color: white;" +
-    "z-index: 999999999;" +
-    "text-align: center;" +
-    "}" +
-    // + on Hover styling
-    "[data-smc-hover='true']{" +
-    "border: 1px solid red;" +
-    "};"
-    // & append to Head elementTag
-    document.head.appendChild(styleTag)
+    style.textContent = "smc-path-box {" +
+        "position: fixed;" +
+        "bottom: 20px;" +
+        "left: 10px;" +
+        "right: 10px;" +
+        "padding: 3px;" +
+        "font-size: 16px;" +
+        "background-color: rgba(87, 12, 12, 0.8);" +
+        "color: white;" +
+        "z-index: 999999999;" +
+        "text-align: center;" +
+        "}" +
+        // Part 2 - Element Hover
+        "[data-smc-hover='true']{" +
+        "border: 1px solid red;" +
+        "};"
+    // & append to head
+    document.head.appendChild(style)
 }
 
+// appends pathBoxElement to body
 function addPathBoxToBody() {
-    // Add pathBoxElement to document body
     const pathBoxElement = document.createElement('smc-path-box')
     pathBoxElement.textContent = `Selector Path:`
     document.body.appendChild(pathBoxElement)
 }
 
+// Part 2 - Element Hover
 function addBorderOnHover() {
-    // select all elements in the DOM object and return HTML collection
-    const elements = document.body.querySelectorAll('*')
-    // convert HTML collection into an array
-    const myArray = Array.from(elements)
+    // select all elements in the DOM object and return the Node List array
+    const elementList = document.body.querySelectorAll('*')
     // Loop over each element and add/remove mouseEvent listener to set/remove attribute on hover
-    myArray.forEach(item => {
-        item.addEventListener('mouseover', (e) => {
-            e.target.setAttribute('data-smc-hover', 'true')
-        })
-        item.addEventListener('mouseout', (e) => {
-            e.target.removeAttribute('data-smc-hover')
-        })
+    elementList.forEach(element => {
+            element.addEventListener('mouseover', (e) => {
+                e.target.setAttribute('data-smc-hover', 'true')
+            })
+            element.addEventListener('mouseout', (e) => {
+                e.target.removeAttribute('data-smc-hover')
+            })
     })
 }
 
+/*  
+    Part 3 - Getting the elements path
+    This section is made up of three composable functions
+    1. addClickListener => listens for the click then passes the event to the next function
+    2. getSelectorPath  => checks possible selector combination paths and passes to next function
+    3. addSelectorpath  => adds the path to the pathBoxBody
+*/
+
+// takes in the selector path & displays it on the PathBox
 function addSelectorPath(path) {
-    const clickedElement = document.getElementsByTagName('smc-path-box')
-    clickedElement[0].textContent = `Selector Path: ${path}`
+    const smcPathBox = document.getElementsByTagName('smc-path-box')[0] // returns an HTML collection with single result
+    smcPathBox.textContent = `Selector Path: ${path}` // returns the selector path to display in the element
 }
 
-function getSelectorPath() {
-    // select all dom elements in the document object and return HTML collection
-    const elements = document.body.querySelectorAll('*')
-    // convert HTML collection into an array
-    const elementsArray = Array.from(elements)
+// takes in the event & finds the css selector path
+function getSelectorPath(event) {
+    // e = element & p = parent
+    const pTag   = event.target.parentElement.nodeName.toLowerCase()
+    const eTag   = event.target.tagName.toString().toLowerCase()
+    const eId    = event.target.id
+    const eClass = event.target.className
+        // checking all selector combinations
+        if(pTag && eTag && eId && eClass)   {addSelectorPath(`${pTag}>${eTag}#${eId}.${eClass}`)}
+        if(pTag && eTag && !eId && eClass)  {addSelectorPath(`${pTag}>${eTag}.${eClass}`)}
+        if(pTag && eTag && eId && !eClass)  {addSelectorPath(`${pTag}>${eTag}#${eId}`)}
+        if(pTag && eTag && !eId && !eClass) {addSelectorPath(`${pTag}>${eTag}`) }
+}
 
-    // on click show css selector path
-        elementsArray.forEach(element => {
+function addClickListener() {
+    // select all elements in the body and return a Node List array
+    const elementList = document.body.querySelectorAll('*')
+    // loops thru elements in the node list & attaches a click listener
+        elementList.forEach(element => {
             element.addEventListener('click', (ev) => {
-
-                ev.stopPropagation()
-                // assign variables to tags/class/ids
-                const parentTag = ev.target.parentElement.nodeName.toLowerCase()
-                const elementTag = ev.target.tagName.toString().toLowerCase()
-                const elementId = ev.target.id
-                const elementClassName = ev.target.className
-
-                // checking all possible combinations for selectors
-                if(parentTag && elementTag && elementId && elementClassName) {
-                    addSelectorPath(`${parentTag}
-                    >${elementTag}
-                    #${elementId}
-                    .${elementClassName}`)
-                }
-                if(parentTag && elementTag && !elementId && elementClassName) {
-                    addSelectorPath(`${parentTag}
-                    >${elementTag}
-                    .${elementClassName}`)
-                }
-                if(parentTag && elementTag && elementId && !elementClassName) {
-                    addSelectorPath(`${parentTag}
-                    >${elementTag}
-                    #${elementId}`)
-                }
-                if(parentTag && elementTag && !elementId && !elementClassName) {
-                    addSelectorPath(`${parentTag}
-                    >${elementTag}`)
-                }
-            })  
+                ev.stopPropagation() // prevent propagation to parent on hover
+                getSelectorPath(ev) // passes the event to fn responsible for mapping selector path 
+            })
         })
 }
 
 // on window load execute functions
-window.onload = function initialiseCode() {
+window.onload = () => {
     addCssToStyleTag()
     addPathBoxToBody()
     addBorderOnHover()
-    getSelectorPath()
+    addClickListener()
 }
